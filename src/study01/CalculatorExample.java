b0ee290ef7ff858fda28c22e08b91a8ff64d6d0c
yeonggeumkang <realyoungm@naver.com>
2019-12-07 02:47:22 +0900

    06 Class : 메소드

---
+++ src/study01/CalculatorExample.java
@@ -0,0 +1,9 @@
+package study01;
+
+public class CalculatorExample {
+	public static void main(String[] args) {
+		Calculator myCal = new Calculator();
+		myCal.powerOn();
+		myCal.powerOff();
+	}
+}
